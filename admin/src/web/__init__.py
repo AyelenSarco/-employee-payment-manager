1a2618755443f0e0cd83bbe28565d887ef1077bb
Ayelen Marina Sarco <ayelensarco94@gmail.com>
2025-05-22 12:02:53 -0300

    create Flask command to seed the database and seed file

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from src.web.config import config
 from src.core import database
-
+from src.core import seeds
 
 def create_app(env="development", static_folder=""):
 
@@ -20,5 +20,8 @@ def create_app(env="development", static_folder=""):
     def reset_db():
         database.reset()
 
+    @app.cli.command(name="seeds-db")
+    def seeds_db():
+        seeds.run()
     
     return app
